17f30da9d1dcd1f03d304823bcb1c5ad20bb9542
Jon Bell <github@lot23.com>
2022-07-03 04:41:06 +0000

    Updates

--- pages/_app.js
+++ pages/_app.js
@@ -11,8 +11,15 @@ function MyApp({ Component, pageProps }) {
         <link rel="apple-touch-icon" sizes="180x180" href="/apple-touch-icon.png" />
         <link rel="icon" type="image/png" sizes="32x32" href="/favicon-32x32.png" />
         <link rel="icon" type="image/png" sizes="16x16" href="/favicon-16x16.png" />
-        <link rel = "shortcut icon" type = "image/x-icon" href = "/favicon.ico" />        
-        <link rel="manifest" href="/site.webmanifest" />        
+        <link rel="shortcut icon" type = "image/x-icon" href = "/favicon.ico" />        
+        <link rel="manifest" href="/site.webmanifest" />
+        
+        <meta name="twitter:card" content="summary_large_image" />
+        <meta name="twitter:creator" content="@jonbell" />
+        <meta name="twitter:title" content="Maybe All That Matters is How You Treat People" />
+        <meta name="twitter:description" content="Remembering Mike Kruzeniski one year later" />
+        <meta name="twitter:image" content="http://maybeallthatmatters.com/tweetcard.png" />
+             
       </Head>      
       <Component {...pageProps} />
     </>
